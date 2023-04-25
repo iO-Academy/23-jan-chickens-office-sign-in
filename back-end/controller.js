f6239ada35a92ecfd8458af6d5991bed7dea8f06
Bill Turvill <turvill@hotmail.co.uk>
2023-04-25 15:11:19 +0100

    validation logic

--- back-end/controller.js
+++ back-end/controller.js
@@ -2,23 +2,32 @@ const { getCollection } = require('./service/DatabaseService')
 
 const addNewVisitor = async (request, response) => {
     const collection = await getCollection("OfficeSignIn", "Visitors")
-    const newVisitor = {
-        name: request.body.name,
-        company: request.body.company,
-        signInTime: request.body.signInTime,
-        signedIn: true
+    let newVisitor
+    if (request.body.name && request.body.signInTime) {
+        newVisitor = {
+            name: request.body.name,
+            signInTime: request.body.signInTime,
+            signedIn: true
+        }
+        console.log("name and signInTime detected")
+        if (request.body.company) {
+            console.log("company detected")
+            newVisitor['company'] = request.body.company
+        }
+        response.status(200).json({
+            message: "Successfully signed in visitor.",
+            data: []
+        })
+    } else {
+        console.log("name and/or signInTime not detected")
+        return response.status(400).json({
+            message: "Invalid data provided",
+            data: []
+        })
     }
-    //request.body.company ? newVisitor[company] = 
-    //Object.keys(request.body).forEach(())
-    console.log("hello, world")
-    console.dir(newVisitor)
+    console.dir("newVisitor: " + JSON.stringify(newVisitor))
     await collection.insertOne(newVisitor)
     console.dir("request.body: " + JSON.stringify(request.body))
-    
-    response.status(200).json({
-        message: "Successfully signed in visitor.",
-        data: []
-    })
 }
 
 module.exports = { addNewVisitor }
