3635716f12e64ce8b48dffaab3e701ae9d51407b
Bill Turvill <turvill@hotmail.co.uk>
2023-04-27 11:01:12 +0100

    getVisitorsBySignIn

--- back-end/index.js
+++ back-end/index.js
@@ -2,37 +2,37 @@ const { routes } = require('./Routes.js')
 const express = require('express')
 const app = express()
 const port = 3001
-const cors = require('cors')
+//const cors = require('cors')
 
 const session = require('express-session')
 const { store } = require('./service/DatabaseService')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-const allowedOrigins = ['http://localhost:3003'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
-app.use((request, response, next) => {
-    response.header('Access-Control-Allow-Credentials', 'true')
-    next()
-})
-app.use((request, response, next) => {
-    response.header('Access-Control-Expose-Headers', 'Set-Cookie')
-    next()
-})  
-  app.use(function(request, response, next) {
-    response.header("Referrer-Policy", "same-origin")
-    next()
-})
+// const allowedOrigins = ['http://localhost:3003'];
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// }
+// app.use(cors(corsOptions))
+// app.use((request, response, next) => {
+//     response.header('Access-Control-Allow-Credentials', 'true')
+//     next()
+// })
+// app.use((request, response, next) => {
+//     response.header('Access-Control-Expose-Headers', 'Set-Cookie')
+//     next()
+// })  
+//   app.use(function(request, response, next) {
+//     response.header("Referrer-Policy", "same-origin")
+//     next()
+// })
   
   
 app.use(session({
--- back-end/routes.js
+++ back-end/routes.js
@@ -2,13 +2,14 @@ const express = require('express')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session);
 const app = express()
-const { addNewVisitor, getAdminAuthorization } = require('./controller.js')
+const { addNewVisitor, getAdminAuthorization, getVisitorsBySignIn } = require('./controller.js')
 const { restrictToAdmin } = require('./middleware/restrictToAdmin')
 
 function routes(app) {
     app.post('/visitors', addNewVisitor)
     app.post('/verify', getAdminAuthorization)
-    app.get('/visitors', restrictToAdmin) //adminPageRoute
+    //app.get('/visitors', restrictToAdmin) //adminPageRoute
+    app.get('/visitors', getVisitorsBySignIn)
 }
 
 module.exports = { routes }
